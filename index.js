1ab3000f2103f657c46b51f50a174db3065271f1
kor <smfwnd9@naver.com>
2020-08-24 01:09:28 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on('message', (message) => {
+  if(message.content === '안녕') {
+    message.reply('안녕하세요');
+  }
+});
+
+client.login(token);
